a0e13c93196b87a2be0168eb09583053a851cb4e
paul kayongo <nserekopaul@gmail.com>
2019-03-01 20:25:58 +0300

    Revert "chore(setup redux): Setting up redux for the project"

--- src/redux/store/combineStore.js
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-
-export default () =>{
-    const store = createStore(combineReducers({}), applyMiddleware(thunk));
-
-    return store;
-}
