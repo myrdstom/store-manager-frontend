5fbc6b2eff79d3c068318a2f0110f78bdedb250e
paul kayongo <nserekopaul@gmail.com>
2019-03-01 20:25:58 +0300

    Revert "chore(setup redux): Setting up redux for the project"

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-import App from './react/components/App';
+import App from './App';
 
-ReactDOM.render(
-    <Provider store={store}>
-    <App />
-    </Provider>,
-    
-    document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
