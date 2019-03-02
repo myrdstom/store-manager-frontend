79ef6d781c06ecaacadd56feb70a75a9ff0bbe92
myrdstom <nserekopaul@gmail.com>
2019-03-02 13:51:08 +0300

    ch:(contninuous intergration): Setting up  circleci and codeclimate [Maintains #164330015]

--- src/react/components/App.js
+++ src/react/components/App.js
@@ -5,7 +5,6 @@ class App extends Component {
     render(){
         return(
         <div>
-            <h1>test</h1>
         </div>
         );
     }
---
+++ src/redux/reducers/mainReducer.js
@@ -0,0 +1,16 @@
+    
+import { combineReducers } from 'redux';
+
+const InitialState = {
+  start: false
+};
+
+function startReducer(state = InitialState) {
+  return state;
+}
+
+const rootReducer = combineReducers({
+  start: startReducer
+});
+
+export default rootReducer;
