588b76882ce232b2570d2b97daaa2e51c7177247
myrdstom <nserekopaul@gmail.com>
2019-03-10 10:44:23 +0300

    Restructuring my file structure

--- src/react/routes/index.js
+++ src/react/routes/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import loginView from '../views/login/LoginView'
-import SignUpView from '../views/signup/SignUpView';
+import loginView from '../components/login/LoginView'
+import SignUpView from '../components/signup/SignUpView';
 import 'react-toastify/dist/ReactToastify.min.css'
-import ProductView from '../views/products/ProductView'
+import ProductView from '../components/products/ProductView'
 
 const Routes = () => (
     <Router>
